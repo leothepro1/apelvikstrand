758c842983289e6ccc33bbf3177c61b143a86d42
leothepro1 <leo@lrstudio.se>
2026-02-03 21:24:54 +0100

    Update apelvikstrand.maps.js

--- public/apelvikstrand.maps.js
+++ public/apelvikstrand.maps.js
@@ -70,7 +70,7 @@
     };
               const sektion73InitialCenter = {
       title: "Apelviken livs",
-      lngLat: [12.253572, 57.085734]
+      lngLat: [12.255780, 57.085314]
     };
                    const sektion73apelviken = {
       title: "Apelviken livs",
@@ -114,7 +114,7 @@ const sektion73Tangkorar_4 = {
 
     const sektion73MinZoom = 13.2;
     const sektion73MaxZoom = 17.9;
-    const sektion73StartZoom = 13.2;
+    const sektion73StartZoom = 16.1;
 const sektion73SecondaryPinsMinZoom = 15.8;
     // Kamera
     const sektion73Pitch = 65;
